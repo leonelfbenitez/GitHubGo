25db40a6802085d942e43a54adc0787f83f74705
Meet Patel <meetnpatel368@gmail.com>
2022-11-10 14:16:09 -0500

    hot fix

--- src/pages/blamefile.js
+++ src/pages/blamefile.js
@@ -7,6 +7,7 @@ const BlameFile = () => {
     return (
         <div className="flex items-center flex-col min-h-screen" onSubmit={handleSubmit}>
             <form className='flex flex-col gap-3 md:flex-row mt-4'>
+                <input spellCheck={false} title="Owner Name" className="bg-gray-50 p-3 rounded-md focus:outline-gray-200" type={'text'} name='ownername' placeholder="Owner Name" />
                 <input spellCheck={false} title="Repository Name" className="bg-gray-50 p-3 rounded-md focus:outline-gray-200" type={'text'} name='reponame' placeholder="Repository Name" />
                 <input spellCheck={false} title="Username" className="bg-gray-50 p-3 rounded-md focus:outline-gray-200" type={'text'} name='username' placeholder="Username" />
                 <input spellCheck={false} title="File path" className="bg-gray-50 p-3 rounded-md focus:outline-gray-200" type={'text'} name='filepath' placeholder="File Path" />
--- src/pages/repostats.js
+++ src/pages/repostats.js
@@ -7,6 +7,7 @@ function RepoStats() {
     return (
         <div className="flex items-center flex-col min-h-screen mt-4" onSubmit={handleSubmit}>
             <form className='flex flex-col gap-3 md:flex-row'>
+                <input spellCheck={false} title="Owner Name" className="bg-gray-50 p-3 rounded-md focus:outline-gray-200" type={'text'} name='ownername' placeholder="Owner Name" />
                 <input spellCheck={false} title="Repository Name" className="bg-gray-50 p-3 rounded-md focus:outline-gray-200" type={'text'} name='reponame' placeholder="Repository Name" />
                 <input spellCheck={false} title="Username" className="bg-gray-50 p-3 rounded-md focus:outline-gray-200" type={'text'} name='username' placeholder="Username" />
                 <button type={"submit"} className="p-3 text-white bg-blue-500 rounded-md focus:outline-blue-700">Submit</button>
